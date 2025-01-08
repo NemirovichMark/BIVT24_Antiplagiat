ccb8d7c66cc6865488cb974d1051d10167f3ca8b
Mark <112826355+NemirovichMark@users.noreply.github.com>
2025-01-09 02:08:49 +0300

    Add files via upload

---
+++ LabsPlagiatTester/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LabsPlagiatTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lab1 = new Lab1("C:\\Users\\User\\source\\repos\\WorksForPlagiat\\Lab1");
+        }
+    }
+}
